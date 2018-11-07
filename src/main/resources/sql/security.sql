518adcc1f00ae464145e41eaf5ac0522c237b774
Armando Soares Sousa <armando.sousa@gmail.com>
2018-11-07 09:13:33 -0300

    Update security.sql
    Foi comentado o trecho de população do banco promocity, pois ele só funciona depois que a aplicação for executada pela primeira vez, visto que as demais tabelas só serão criadas no momento que a aplicação for executada a primeira vez.

--- src/main/resources/sql/security.sql
+++ src/main/resources/sql/security.sql
@@ -22,12 +22,11 @@ ALTER TABLE users ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 ALTER TABLE users AUTO_INCREMENT=1;
 
 --Populate promotion and coupon tables
-insert promotion values(null, 'Promocao 1', CURDATE(), CURDATE()+1);
-insert coupon values(null, 'Cupom 1 da promocao 1', 10, '1022112233');
-insert promotion_coupons values(1,1)
-insert promotion values(null, 'Promocao 2', CURDATE()+1, CURDATE()+2);
-insert coupon values(null, 'Cupom 2 da promocao 2', 20, '20221122332');
-insert promotion_coupons values(2,2)
-
-insert store values(null, 'Rua Teste 1', 'Cidade teste1', 0, 0, 'Loja 1', 0, 'PI');
-insert store_promotion_list values(1,1);
+--insert promotion values(null, 'Promocao 1', CURDATE(), CURDATE()+1);
+--insert coupon values(null, 'Cupom 1 da promocao 1', 10, '1022112233');
+--insert promotion_coupons values(1,1)
+--insert promotion values(null, 'Promocao 2', CURDATE()+1, CURDATE()+2);
+--insert coupon values(null, 'Cupom 2 da promocao 2', 20, '20221122332');
+--insert promotion_coupons values(2,2)
+--insert store values(null, 'Rua Teste 1', 'Cidade teste1', 0, 0, 'Loja 1', 0, 'PI');
+--insert store_promotion_list values(1,1);
