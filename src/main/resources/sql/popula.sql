dd8749eee95db52d2328f9ae308d053d6f0cb8f4
Armando Soares Sousa <armando.sousa@gmail.com>
2018-11-07 21:17:09 -0300

    Update popula.sql
    Foi atualizado o script para inserir o usuário e os dados do admin.

--- src/main/resources/sql/popula.sql
+++ src/main/resources/sql/popula.sql
@@ -1,3 +1,10 @@
+--Insert and update admin data
+insert users values('armando', '123456', true, null, 'armando@ufpi.edu.br', 0,0);
+update users
+set password='$2a$10$m5hVTpkyjUc8d5vKiNqKAOlLywOe11nw.QIo1dxh7DiUvMLg4VOMy'
+where id=1
+insert authorities values('armando', 'USER');
+
 --Populate promotion and coupon tables
 insert promotion values(null, 'Promocao 1', CURDATE(), CURDATE()+1);
 insert coupon values(null, 'Cupom 1 da promocao 1', 10, '1022112233');
