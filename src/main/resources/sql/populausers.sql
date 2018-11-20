5691f8dadc143a1ae5e9fe6e4d97cd527c58141c
Armando Soares Sousa <armando.sousa@gmail.com>
2018-11-20 12:33:34 -0300

    Create populausers.sql

---
+++ src/main/resources/sql/populausers.sql
@@ -0,0 +1,5 @@
+insert users values('armando', '123456', true, null);
+update users
+set password='$2a$10$m5hVTpkyjUc8d5vKiNqKAOlLywOe11nw.QIo1dxh7DiUvMLg4VOMy'
+where id=1;
+insert authorities values('armando', 'USER');
