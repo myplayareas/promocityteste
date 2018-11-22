3866a63910abdebaf334a07396b7ae27cd02fd03
armandossrecife <armando.sousa@gmail.com>
2018-11-22 11:59:41 -0300

    Correções do bug de carregamento de cupons para um usuário.

--- src/main/resources/sql/populausers.sql
+++ src/main/resources/sql/populausers.sql
@@ -1,4 +1,4 @@
-insert users values('armando', '123456', true, null);
+insert users values('armando', 'armando', true, null);
 update users
 set password='$2a$10$m5hVTpkyjUc8d5vKiNqKAOlLywOe11nw.QIo1dxh7DiUvMLg4VOMy'
 where id=1;
